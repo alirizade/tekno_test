ce997850c978fc5dd157100b7fbfa5f86d267648
emre-yilmaz <emre.yilmaz10@ogr.sakarya.edu.tr>
2014-12-12 15:05:36 +0200

    main degişimi

--- src/tekno_stest/cüneyt.java
+++ src/tekno_stest/cüneyt.java
@@ -2,7 +2,8 @@ package tekno_stest;
 
 public class cüneyt {
 	public int ads(int x){
-		return 1;
+		return 1;}
+		
 		public int adds(int x){
 			return 1;
 	}
---
+++ src/tekno_stest/test_ekrani.java
@@ -0,0 +1,9 @@
+package tekno_stest;
+
+public class test_ekrani {
+
+	public static void main(String[] args) {
+		System.out.println("ali ata bak");
+	}
+
+}
